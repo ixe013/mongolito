704f5c539edad31d19d4c130a0f382dd47e92d6b
ixe013 <guillaume@paralint.com>
2013-06-12 00:54:32 -0400

    Skeleton of a LDAP input

---
+++ test/test_readldap.py
@@ -0,0 +1,10 @@
+import unittest
+
+import readLDAP
+
+class MyTest(unittest.TestCase):
+    def testParseURI(self):
+        pass #readLDAP.LDAPReader.create_from_uri():
+
+if __name__ == "__main__":
+    unittest.main()
