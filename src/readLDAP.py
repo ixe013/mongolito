1ceb6addba1ad7fca3f9bb879447b358337b57fd
ixe013 <guillaume@paralint.com>
2013-06-20 00:33:14 -0400

    Trying to convert query

--- src/readLDAP.py
+++ src/readLDAP.py
@@ -1,25 +1,33 @@
 import ldap
 import ldapurl
 import re
+import utils
 
 class LDAPReader(object):
 
-    def __init__(self, uri, user, password):
+    def __init__(self, uri):
         #FIXME : try and handle ValueError ?
-        uri.replace(',','%2c')
-        ldap_url = ldap.LDAPUrl(uri)
+        ldap_url = ldapurl.LDAPUrl(uri)
+
+        #Will be use to understand queries with mongolito metadata later
         self.base = ldap_url.dn
+        self.user = ldap_url.who
+        self.password = ldap_url.cred
 
-        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+        #Encoding is not automatic, for some reason
+        regex = re.compile("^<(.*)>(.*)<(.*)>$")
+        r = regex.search(ldap_url.htmlHREF())
+        uri = r.group(2)
 
-        self.connection = ldap.ldapobject.LDAPObject('{0}://{1}:{2}'.format('ldaps',host, 636))
+        self.connection = ldap.ldapobject.LDAPObject(uri)
 
-        self.connection.set_option(ldap.OPT_REFERRALS, 0)
-        self.connection.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
-        self.connection.set_option(ldap.OPT_X_TLS,ldap.OPT_X_TLS_DEMAND)
-        self.connection.set_option(ldap.OPT_X_TLS_DEMAND, True )
+        if ldap_url.urlscheme == 'ldaps':
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
-        self.connection.simple_bind_s(user, password)
+            self.connection.set_option(ldap.OPT_REFERRALS, 0)
+            self.connection.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
+            self.connection.set_option(ldap.OPT_X_TLS,ldap.OPT_X_TLS_DEMAND)
+            self.connection.set_option(ldap.OPT_X_TLS_DEMAND, True )
 
 
     @staticmethod
@@ -31,7 +39,10 @@ class LDAPReader(object):
     def create(args):
         return LDAPReader(args.uri)
 
-    def release(self):
+    def connect(self):
+        self.connection.simple_bind_s(self.user, self.password)
+
+    def disconnect(self):
         try:
             self.connection.unbind_s()
         except ldap.LDAPError:
@@ -48,9 +59,14 @@ class LDAPReader(object):
         http://docs.mongodb.org/manual/reference/connection-string/        
 
         '''
-        result = None
+        parsed_url = ldapurl.LDAPUrl(uri)
 
-        return result
+        #Encoding is not automatic, for some reason
+        regex = re.compile("^<(.*)>(.*)<(.*)>$")
+        r = regex.search(parsed_url.htmlHREF())
+        uri = r.group(2)
+
+        return LDAPReader(uri)
 
         
     def get_attribute(self, query={}, attribute = 'dn', error=KeyError):
@@ -62,6 +78,57 @@ class LDAPReader(object):
                 if error is not None:
                     raise ke
 
+    def convert_query(self, query):
+        result = ''
+        base = self.base
+
+        #convert objectClass first
+        try:
+            classes = query['objectClass']
+
+            if isinstance(classes, list):
+                for objclass in classes:
+                    result += '(objectClass={0})'.format(objclass)
+            else:
+                result += '(objectClass={0})'.format(classes)
+    
+            del query['objectClass']
+            
+        except KeyError:
+            #We don't care about objectClass, unusual but valid
+            pass
+            
+        #Then metadata
+        #each in its own try except block
+        #first the rdn
+        try:
+            rdn = query['mongolito.rdn']
+            result += '(|(cn={0})(uid={0}))'.format(rdn)
+        except KeyError:
+            pass
+
+        #then the path
+        try:
+            path = query['mongolito.path']
+            #'/^c=ca,st=qc,o=hydro-quebec,ou=applications,ou=sap,ou=codes_applic/',
+            temp_base = utils.pattern_from_javascript(path) 
+            #'^c=ca,st=qc,o=hydro-quebec,ou=applications,ou=sap,ou=codes_applic',
+            #We are replacing the base
+            #Look at something along those lines
+            #http://stackoverflow.com/a/14128905/591064
+            
+        except KeyError:
+            pass
+
+        #Convert other attributes
+        for k, v in query.items():
+            if isinstance(v, list):
+                for objclass in v:
+                    result += '({0}={1})'.format(k, objclass)
+            else:
+                result += '({0}={1})'.format(k,v)
+            
+        return base, r'(&{0})'.format(result)
 
     def search(self, query = {}, attributes=[]):
         '''Thin wrapper over pymongo.collection.find'''
--- test/test_readldap.py
+++ test/test_readldap.py
@@ -1,10 +1,52 @@
 import unittest
+import collections
 
 import readLDAP
 
 class MyTest(unittest.TestCase):
-    def testParseURI(self):
-        pass #readLDAP.LDAPReader.create_from_uri():
+    def testCreate(self):
+        reader = readLDAP.LDAPReader.create_from_uri('', 'ldap://192.168.2.151/dc=directory,dc=example,dc=com')
+
+    def testConvertQuery(self):
+        reader = readLDAP.LDAPReader.create_from_uri('', 'ldap://192.168.2.151/dc=directory,dc=example,dc=com')
+
+        query = collections.OrderedDict()
+
+        query['objectClass'] = ['top','user']
+        query['cn'] = 'dude'
+        query['loginDisabled'] = 'TRUE'
+
+        base, filt = reader.convert_query(query)
+
+        self.assertEqual(base, 'dc=directory,dc=example,dc=com')
+        self.assertEqual(filt, r'(&(objectClass=top)(objectClass=user)(cn=dude)(loginDisabled=TRUE))')
+
+    def testConvertMetadataQuery(self):
+        reader = readLDAP.LDAPReader.create_from_uri('', 'ldap://192.168.2.151/dc=directory,dc=example,dc=com')
+
+        query = collections.OrderedDict()
+
+        query['objectClass'] = ['top','user']
+        query['mongolito.rdn'] = 'dude'
+        query['mongolito.parent'] = 'ou=users'
+        query['mongolito.path'] = 'dc=com,dc=example,dc=directory'
+
+        base, filt = reader.convert_query(query)
+
+        self.assertEqual(base, r'dc=directory,dc=example,dc=com')
+        self.assertEqual(filt, r'(&(objectClass=top)(objectClass=user)(|(cn=Dude)(uid=Dude)))')
+
+        query = collections.OrderedDict()
+
+        query['objectClass'] = ['top','user']
+        query['mongolito.rdn'] = 'dude'
+        query['mongolito.parent'] = 'ou=users'
+        query['mongolito.path'] = '/^dc=com,dc=example'
+
+        base, filt = reader.convert_query(query)
+
+        self.assertEqual(base, r'dc=example,dc=com')
+        self.assertEqual(filt, r'(&(objectClass=top)(objectClass=user)(|(cn=Dude)(uid=Dude)))')
 
 if __name__ == "__main__":
     unittest.main()
